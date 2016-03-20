199abe7fc395efd7b05b91bd2f2e941458b03067
Jorey Dixon <Jorey.dixon123@gmail.com>
2016-03-19 22:45:38 -0600

    Created the ClickMe Script to change color upon click.

---
+++ Assets/Scripts/ClickMe.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickMe : MonoBehaviour {
+
+	private float one;
+	private float two;
+	private float three;
+
+	void Start()
+	{
+		
+	}
+
+	void OnMouseUp()
+	{
+		one = Random.Range (0.0f, 1.0f);
+		two = Random.Range (0.0f, 1.0f);
+		three = Random.Range (0.0f, 1.0f);
+
+		this.GetComponent<SpriteRenderer> ().color = new Color (one, two, three);
+	}
+}
